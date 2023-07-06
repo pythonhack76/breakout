e4662a8612f488c31b72d0e53059551dff84a1fd
pythonhack76 <lrulvoni@gmail.com>
2023-07-06 15:22:37 +0200

    update spazio vitale per player sul bottom

--- breakout.js
+++ breakout.js
@@ -14,13 +14,51 @@ let version = "1.0.0";
 //player
 let playerWidth = 80;
 let playerHeight = 10;
+let playerVelocityX = 10;
+
+
 
 let player = {
-    x : boardWidth/2 - playerWidth
+    x : boardWidth/2 - playerWidth/2,
+    y : boardHeight - playerHeight - 5,
+    width : playerWidth,
+    height : playerHeight,
+    velocityX : playerVelocityX
+
 }
 
 window.onload = function() {
         board = document.getElementById("board");
         board.height = boardHeight;
         board.width = boardWidth;
+        context = board.getContext("2d"); 
+
+        //draw the initial player
+        context.fillStyle = "lightgreen";
+        context.fillRect(player.x, player.y, player.width, player.height );
+
+        requestAnimationFrame(update);
+        document.addEventListener("keydown", movePlayer);
+
+
+}
+
+function update(){
+    requestAnimationFrame(update);
+
+    //player
+    context.fillStyle = "lightgreen";
+    context.fillRect(player.x, player.y, player.width, player.height );
+
+
+
+}
+
+function movePlayer(e) {
+    if (e.code == "ArrowLeft"){
+        player.x -= playerVelocityX;
+    }
+    else if (e.code == "ArrowRight"){
+        player.x += player.velocityX;
+    }
 }
